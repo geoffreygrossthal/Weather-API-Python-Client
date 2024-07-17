dcff62e9becd9e48193d677b0b0ca28aad815571
Geoffrey Grossthal <104024651+geoffreygrossthal@users.noreply.github.com>
2024-07-16 19:35:30 -0700

    Update WeatherAPI.py

--- WeatherAPI.py
+++ WeatherAPI.py
@@ -1,32 +1,25 @@
-name: Pylint
+@@ -1,25 +1,32 @@
+import requests
 
-on:
-  push:
-    branches:
-      - main
+def get_weather(api_key, location, aqi='no'):
+    base_url = "http://api.weatherapi.com/v1/current.json"
+    query = {'key': api_key, 'q': location, 'aqi': aqi}
 
-jobs:
-  build:
-    runs-on: windows-latest  # Specify Windows environment
+    response = requests.get(base_url, params=query)
 
-    strategy:
-      matrix:
-        python-version: ["3.8", "3.9", "3.10"]
+    if response.status_code == 200:
+        data = response.json()
+        # Print relevant information from the response
+        print(f"Weather in {data['location']['name']}, {data['location']['country']}:")
+        print(f"Temperature: {data['current']['temp_c']} degrees Celsius")
+        print(f"Condition: {data['current']['condition']['text']}")
+    else:
+        print(f"Failed to retrieve weather data: {response.status_code}")
+        print(response.text)  # Print the response text for further debugging
 
-    steps:
-      - name: Checkout code
-        uses: actions/checkout@v2
+if __name__ == "__main__":
+    # Replace 'YOUR_API_KEY' with your actual WeatherAPI.com API key
+    api_key = '3a6e356b738a4199b99151954241107'
+    location = 'London'
 
-      - name: Set up Python ${{ matrix.python-version }}
-        uses: actions/setup-python@v2
-        with:
-          python-version: ${{ matrix.python-version }}
-
-      - name: Install dependencies
-        run: |
-          python -m pip install --upgrade pip
-          pip install pylint
-
-      - name: Analysing the code with pylint
-        run: |
-          pylint $(git ls-files '*.py')
+    get_weather(api_key, location)
